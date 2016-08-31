effd02c5d33fb28c80877a069906533da6dee5a3
Nicholas Sofroniew <sofroniewn@gmail.com>
2016-08-31 17:03:21 -0400

    init commit

--- example.js
+++ example.js
@@ -1,7 +1,9 @@
-var experiment = require('./')
-var device = require('device-stream-mvr-stdin').createStream()
+var Experiment = require('./')
+var device = require('device-stream-mvr-stdin')()
+var deviceStream = device.createStream()
+device.start()
 
-map = {
+var map = {
   area: [[-15, 0], [-15, 20], [-45.2, 47.7], [-45.2, 57.7],
     [-15.3, 57.7], [-15.3, 47.7], [0, 33.7], [15.3, 47.7], [15.3, 57.7],
     [-15, 85.4], [-15, 95.4], [15, 95.4], [15, 85.4], [30.3, 71.4],
@@ -27,12 +29,63 @@ map = {
   ]
 }
 
-var expt = experiment.createStream(map)
+var mapNEW = {
+  area: [[-15, 0], [-15, 20], [-45.2, 47.7], [-45.2, 57.7],
+    [-15.3, 57.7], [-15.3, 47.7], [0, 33.7], [15.3, 47.7], [15.3, 57.7],
+    [-15, 85.4], [-15, 95.4], [15, 95.4], [15, 85.4], [30.3, 71.4],
+    [45.6, 85.4], [45.6, 95.4], [75.6, 95.4], [75.6, 85.4], [45.3, 57.7],
+    [45.3, 47.7], [15, 20], [15, 0], [-15, 0]],
+  borders: [
+    [[-15, 0], [-15, 20], [-45.2, 47.7], [-45.2, 57.7],
+      [-15.3, 57.7], [-15.3, 47.7], [0, 33.7], [15.3, 47.7], [15.3, 57.7],
+      [-15, 85.4], [-15, 95.4]],
+    [[15, 95.4], [15, 85.4], [30.3, 71.4], [45.6, 85.4], [45.6, 95.4],
+      [75.6, 95.4], [75.6, 85.4], [45.3, 57.7],
+      [45.3, 47.7], [15, 20], [15, 0]]
+    ],
+  links: [
+      [[-15, 95.4], [15, 95.4]], [[-15, 0], [15, 0]],
+    ],
+  triggers: [
+    [[-100, -100], [-100, 100], [100, 100], [100, -100]]
+  ],
+  playerStart: [0, 5],
+  playerShape: [
+    [-2,-1.5], [0, 1.5], [2, -1.5]
+  ]
+}
 
-var results = device.pipe(expt)
-results.on('data', console.log)
-//results.pipe(device)
+var exp = Experiment()
 
+var eStream = exp.createStream()
+exp.initTrial(map)
+var results = deviceStream.pipe(eStream)
+//results.on('data', console.log)
+results.pipe(deviceStream)
+// results.on('data', function (data) {
+//   console.log(data.reward)
+// })
+
+process.stdin.on('data', function(data) {
+  if (data.toString().trim() === 'n') {
+    exp.updateTrial(mapNEW)
+  }
+})
+
+
+var expState = true
+process.stdin.on('data', function(data) {
+  if (data.toString().trim() === 'p') {
+    if (expState) {
+      console.log('stopping')
+      device.stop()
+    } else {
+      console.log('starting')
+      device.start()
+    }
+    expState = !expState
+  }
+})
 
 //////////////////////////////////////////////////////////////////
 // make so when inside reward zone reward flag is set to true
--- index.js
+++ index.js
@@ -3,99 +3,196 @@ var distance = require('ray-to-lines')
 var collision = require('line-to-lines')
 var inside = require('point-in-polygon')
 var math = require('mathjs')
+var from = require('from2')
 
-module.exports = {
-  createStream: function (map) {
-    var position = map.playerStart
-    var trialNumber = 0
-    var prevTime = Date.now()
-    var reward = false
-    var angles = [0, 90, 180]
-
-    function move(position, delta, map){
-      // do collision on links
-      var link = collision([position, [position[0] + delta[0], position[1] + delta[1]]], map.links)        
-      if (link.component !== false) {
-        if (link.component%2===0) {
-          position[0] -= (map.links[link.component][0][0] - map.links[link.component+1][0][0])
-          position[1] -= (map.links[link.component][0][1] - map.links[link.component+1][0][1])
-          trialNumber++
-        } else {
-          position[0] -= (map.links[link.component][0][0] - map.links[link.component-1][0][0])
-          position[1] -= (map.links[link.component][0][1] - map.links[link.component-1][0][1])
-          trialNumber--
-        }
-      }
-
-      // do collision detection on boundary
-      var hit = collision([position, [position[0] + delta[0], position[1] + delta[1]]], map.borders)
-      if (hit.id !== false) {
-        var scale = (hit.distance-.1)/math.norm(delta)
-        position[0] += delta[0]*scale
-        position[1] += delta[1]*scale
-      } else {
-        position[0] += delta[0]
-        position[1] += delta[1]
-      }
-      return position
+function convertMap (maze) {
+  var map = {}
+  map['area'] = []
+  maze.area.forEach(function (el) {
+    var line = []
+    for (var i = 0; i < el.x.length; i++) {
+      line.push([el.x[i], el.y[i]])
     }
+    map['area'].push(line)
+  })
+  map['borders'] = []
+  maze.borders.forEach(function (el) {
+    var line = []
+    for (var i = 0; i < el.x.length; i++) {
+      line.push([el.x[i], el.y[i]])
+    }
+    map['borders'].push(line)
+  })
+  map['triggers'] = []
+  maze.triggers.forEach(function (el) {
+    var line = []
+    for (var i = 0; i < el.x.length; i++) {
+      line.push([el.x[i], el.y[i]])
+    }
+    map['triggers'].push(line)
+  })
+  map['links'] = []
+  maze.links.forEach(function (el) {
+    map['links'].push([[el.x[0], el.y[0]], [el.x[1], el.y[1]]], [[el.x[2], el.y[2]], [el.x[3], el.y[3]]])
+  })
+  map['name'] = maze.name
+  return map
+}
 
-    return through.obj(function (data, enc, callback) {
-      var curTime = Date.now()
-      var deltaTime = curTime - prevTime
-      prevTime = curTime
-      
-      var delta = [data.velocity.lateral, data.velocity.forward]
-      position = move(position, delta, map)
-      
-      var wallDistance = [0, 0, 0]
-      var hit = [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
-      
-      // measure distances to obstacles
-      angles.forEach(function (ang, i) {
-        var start = position
-        var hitI = distance(start, ang, map.borders)
-        var link = distance(start, ang, map.links)
-        if (hitI.distance > link.distance) {
-          wallDistance[i] += link.distance
-          hit[i][0] = [[start[0], start[1]], [link.intersection[0], link.intersection[1]]]
+module.exports = function () {
+  var map = null
+  var nextMap = null
+  var trialStream = from.obj(function () {})
+  var position = null
+  var trialNumber = null
+  var startTime = null
+  var advancedFlag = false
+  var playerStart = [0, .5]
+
+  return {
+    createStream: function () {
+      var prevTime = Date.now()
+      var reward = false
+      var angles = [0, 90, 180]
+      var collisionFlag = false
+      var linkedFlag = false
+
+      function move(position, delta){
+        // do collision on links
+        var link = collision([position, [position[0] + delta[0], position[1] + delta[1]]], map.links)        
+        if (link.component !== false) {
           if (link.component%2===0) {
-            start = [link.intersection[0] - (map.links[link.component][0][0] - map.links[link.component+1][0][0]), link.intersection[1] - (map.links[link.component][0][1] - map.links[link.component+1][0][1])]
+            position[0] -= (map.links[link.component][0][0] - map.links[link.component+1][0][0])
+            position[1] -= (map.links[link.component][0][1] - map.links[link.component+1][0][1])
+            trialNumber++
+            map = nextMap[0]
+            linkedFlag = true
+            trialStream.push({
+              map: map,
+              trialNumber: trialNumber
+            })
           } else {
-            start = [link.intersection[0] - (map.links[link.component][0][0] - map.links[link.component-1][0][0]), link.intersection[1] - (map.links[link.component][0][1] - map.links[link.component-1][0][1])]
+            // position[0] -= (map.links[link.component][0][0] - map.links[link.component-1][0][0])
+            // position[1] -= (map.links[link.component][0][1] - map.links[link.component-1][0][1])
+            // trialNumber--
+            var scale = (link.distance-.1)/math.norm(delta)
+            position[0] += delta[0]*scale
+            position[1] += delta[1]*scale
+            delta[0] = 0
+            delta[1] = 0
+            collisionFlag = true
           }
-          hitI = distance(start, ang, map.borders)
-        } else {
-          hit[i][0] = [[start[0], start[1]], [start[0], start[1]]]
         }
-          wallDistance[i] += hitI.distance
-          hit[i][1] = [[start[0], start[1]], [hitI.intersection[0], hitI.intersection[1]]]
-      })
 
-      reward = false
-      map.triggers.forEach(function (poly) {
-        if (inside(position, poly)) reward = true
-      })
+        // do collision detection on boundary
+        var hit = collision([position, [position[0] + delta[0], position[1] + delta[1]]], map.borders)
+        if (hit.id !== false) {
+          var scale = (hit.distance-.1)/math.norm(delta)
+          position[0] += delta[0]*scale
+          position[1] += delta[1]*scale
+          collisionFlag = true
+        } else {
+          position[0] += delta[0]
+          position[1] += delta[1]
+        }
+        return position
+      }
 
-      callback(null, {
-        position: {
-          forward: position[1],
-          lateral: position[0],
-        },
-        velocity: data.velocity,
-        response: data.response,
-        reward: reward,
+      return through.obj(function (data, enc, callback) {
+        if (startTime === null) {
+          startTime = Date.now()
+        }
+        var curTime = Date.now()
+        var deltaTime = curTime - prevTime
+        prevTime = curTime
+        var elapsedTime = curTime - startTime
+
+        collisionFlag = false
+        linkedFlag = false
+        var delta = [data.velocity.lateral, data.velocity.forward]
+        position = move(position, delta)
+        
+        var wallDistance = [0, 0, 0]
+        var hit = [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
+        
+        // measure distances to obstacles
+        angles.forEach(function (ang, i) {
+          var start = position
+          var hitI = distance(start, ang, map.borders)
+          var link = distance(start, ang, map.links)
+          if (hitI.distance > link.distance) {
+            wallDistance[i] += link.distance
+            hit[i][0] = [[start[0], start[1]], [link.intersection[0], link.intersection[1]]]
+            if (link.component%2===0) {
+              start = [link.intersection[0] - (map.links[link.component][0][0] - map.links[link.component+1][0][0]), link.intersection[1] - (map.links[link.component][0][1] - map.links[link.component+1][0][1])]
+            } else {
+              start = [link.intersection[0] - (map.links[link.component][0][0] - map.links[link.component-1][0][0]), link.intersection[1] - (map.links[link.component][0][1] - map.links[link.component-1][0][1])]
+            }
+            hitI = distance(start, ang, map.borders)
+          } else {
+            hit[i][0] = [[start[0], start[1]], [start[0], start[1]]]
+          }
+            wallDistance[i] += hitI.distance
+            hit[i][1] = [[start[0], start[1]], [hitI.intersection[0], hitI.intersection[1]]]
+        })
+
+        reward = false
+        map.triggers.forEach(function (poly) {
+          if (inside(position, poly)) reward = true
+        })
+
+        callback(null, {
+          position: {
+            forward: position[1],
+            lateral: position[0],
+          },
+          velocity: data.velocity,
+          response: data.response,
+          reward: reward,
+          trialNumber: trialNumber,
+          wallDistance: {
+            right: wallDistance[0],
+            forward: wallDistance[1],
+            left: wallDistance[2],
+          },
+          link: linkedFlag,
+          collision: collisionFlag,
+          advance: advancedFlag,
+          deltaTime: deltaTime,
+          elapsedTime: elapsedTime,
+          time: curTime,
+          hit: hit
+        })
+        advancedFlag = false
+      })
+    },
+    updateTrial: function(newMap) {
+      nextMap = [convertMap(newMap)]
+    },
+    advanceTrial: function() {
+      trialNumber++
+      map = nextMap[0]
+      position = [playerStart[0], playerStart[1]]
+      advancedFlag = true
+      trialStream.push({
+        map: map,
         trialNumber: trialNumber,
-        wallDistance: {
-          right: wallDistance[0],
-          forward: wallDistance[1],
-          left: wallDistance[2],
-        },
-        //link: link,
-        //collision: collision,
-        time: deltaTime,
-        hit: hit
+        init: false
       })
-    })
+    },
+    initTrial: function(newMap) {
+      trialNumber = 0
+      map = convertMap(newMap)
+      nextMap = [convertMap(newMap)]
+      startTime = null
+      position = [playerStart[0], playerStart[1]]
+      advancedFlag = true
+      trialStream.push({
+        map: map,
+        trialNumber: trialNumber,
+        init: true,
+      })
+    },
+    trialStream: trialStream
   }
 }
