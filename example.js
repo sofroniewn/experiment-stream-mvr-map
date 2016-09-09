abc5ec90712dbc6a81b47435f4464ef14360b739
Nicholas Sofroniew <sofroniewn@gmail.com>
2016-09-09 00:14:00 -0400

    update

--- example.js
+++ example.js
@@ -1,15 +1,23 @@
 var experiment = require('./')()
 var device = require('../tinberg-dev-mvr-stdin')()
 var deviceStream = device.createStream()
+var program = require('commander')
 
+program
+  .option('-s, --session [number]', 'Number of session', 0)
+  .option('-l, --logging [bool]', 'State of logging', false)
+  .parse(process.argv)
+
+
+var trialStream = experiment.trialStream
 var throughStream = experiment.createBehaviorStream()
-var resultsStream = deviceStream.pipe(throughStream)
-resultsStream.pipe(deviceStream)
-experiment.trialStream.on('data', console.log)
+var behaviorStream = deviceStream.pipe(throughStream)
+behaviorStream.pipe(deviceStream)
+//trialStream.on('data', console.log)
 
 
 var trials = experiment.getTrials()
-experiment.setCurrentTrial(trials[0])
+experiment.startTrial(trials[0])
 experiment.setNextTrial(trials[1])
 
 
@@ -20,7 +28,6 @@ process.stdin.on('data', function(data) {
     experiment.advanceTrial()
   }
 })
-
 
 var expState = false
 process.stdin.on('data', function(data) {
@@ -35,3 +42,25 @@ process.stdin.on('data', function(data) {
     expState = !expState
   }
 })
+
+
+var mkdirp = require('mkdirp')
+var leftPad = require('left-pad')
+var logging = require('time-stream')
+
+var encoders = experiment.getEncoders()
+var loggingFlag = program.logging
+var sessionNumber = program.session
+var loggingDataStream = null
+var loggingTrialStream = null
+if (loggingFlag) {
+  console.log('start logs')
+  var savePath = './logs/' + leftPad(sessionNumber, 6, 0) + '/behavior'
+  mkdirp(savePath)
+  if (loggingDataStream !== null) behaviorStream.unpipe(loggingDataStream)
+  loggingDataStream = logging.createWriteStream(savePath + '/behavior.data', encoders.behavior.Data)
+  behaviorStream.pipe(loggingDataStream)
+  if (loggingTrialStream !== null) trialStream.unpipe(loggingTrialStream)
+  loggingTrialStream = logging.createWriteStream(savePath + '/trial.data', encoders.trial.Data)
+  trialStream.pipe(loggingTrialStream)
+}
