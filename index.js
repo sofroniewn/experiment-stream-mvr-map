b308988a0f05e3a0176af6ecf9c7c6d501634887
Nicholas Sofroniew <sofroniewn@gmail.com>
2016-09-17 11:17:45 -0400

    move create function

--- index.js
+++ index.js
@@ -1,29 +1,28 @@
 var visualization = require('./visualization')
-var create = require('./streams')
+var create = require('./create')
+
 var glob = require('glob')
 var jsonfile = require('jsonfile')
 var protoBuf = require('protocol-buffers')
 var fs = require('fs')
 
 
-module.exports = function () {
-  return {
-    create: create,
-    visualization: visualization,
-    getTrials: function () {
-      var trials = {}
-      var files = glob.sync(__dirname + '/trials/*.maze', [])
-      files.forEach(function (el) {
-        obj = jsonfile.readFileSync(el)[0]
-        trials[obj.name] = obj
-      })
-      return trials
-    },
-    getEncoders: function () {
-      return {
-        behavior: protoBuf(fs.readFileSync(__dirname + '/proto/behavior.proto')),
-        trial: protoBuf(fs.readFileSync(__dirname + '/proto/trial.proto'))
-      }
+module.exports = {
+  create: create,
+  visualization: visualization,
+  trials: function () {
+    var trials = {}
+    var files = glob.sync(__dirname + '/trials/*.maze', [])
+    files.forEach(function (el) {
+      obj = jsonfile.readFileSync(el)[0]
+      trials[obj.name] = obj
+    })
+    return trials
+  },
+  encoders: function () {
+    return {
+      behavior: protoBuf(fs.readFileSync(__dirname + '/proto/behavior.proto')),
+      trial: protoBuf(fs.readFileSync(__dirname + '/proto/trial.proto'))
     }
   }
 }
